3007f47293e4ae032275ba7bd009d1985f3519f7
kieljulian <julian.kiel@gmail.com>
2025-03-18 15:23:32 +0100

    Add more team members

--- components/sections/TeamSection.vue
+++ components/sections/TeamSection.vue
@@ -53,6 +53,41 @@ const teamMembers = [
     // description: 'Marion kümmert sich um administrative Aufgaben und sorgt für einen reibungslosen Praxisablauf. Sie ist oft Ihre erste Ansprechpartnerin in unserer Praxis.',
     // specialties: ['Patientenbetreuung', 'Organisation'],
     image: '/img/team/Marion-Schmidt.jpg'
+  },
+  {
+    name: 'Maria Garcia',
+    position: 'Ernährungsberaterin',
+    // description: 'Maria unterstützt unsere Patienten mit personalisierten Ernährungsplänen und berät zu gesunder Lebensweise.',
+    // specialties: ['Ernährungsberatung', 'Diätetik'],
+    image: '/img/team/maria-garcia.jpg'
+  },
+  {
+    name: 'Dr. Albert Dera',
+    position: 'Neurologe',
+    // description: 'Dr. Dera ist spezialisiert auf die Diagnose und Behandlung von neurologischen Erkrankungen mit besonderem Fokus auf nicht-invasive Therapiemethoden.',
+    // specialties: ['Neurologie', 'Schmerztherapie'],
+    image: '/img/team/albert-dera.jpg'
+  },
+  {
+    name: 'Polova Hans',
+    position: 'Physiotherapeut',
+    // description: 'Polova verfügt über umfangreiche Erfahrung in der Rehabilitation und unterstützt unsere Patienten bei der Wiederherstellung ihrer Mobilität.',
+    // specialties: ['Physiotherapie', 'Sportrehabilitation'],
+    image: '/img/team/polova-hans.jpg'
+  },
+  {
+    name: 'Jurica Mensen',
+    position: 'Psychotherapeut',
+    // description: 'Jurica bietet psychologische Unterstützung und Therapie für Patienten mit verschiedenen psychischen Herausforderungen.',
+    // specialties: ['Psychotherapie', 'Stressbewältigung'],
+    image: '/img/team/jurica-mensen.jpg'
+  },
+  {
+    name: 'Darshan Patel',
+    position: 'Orthopäde',
+    // description: 'Dr. Patel ist Experte für Erkrankungen des Bewegungsapparates und berät zu konservativen und operativen Behandlungsmöglichkeiten.',
+    // specialties: ['Orthopädie', 'Sportmedizin'],
+    image: '/img/team/darshan-patel.jpg'
   }
 ];
 </script>
