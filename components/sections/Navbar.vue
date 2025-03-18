cb51e8e6a4f0e0b57caf8580284ce9161934bcb0
kieljulian <julian.kiel@gmail.com>
2025-03-18 23:50:38 +0100

    Change logo size and remove second navbar dropdown close button

--- components/sections/Navbar.vue
+++ components/sections/Navbar.vue
@@ -41,12 +41,6 @@
       
       <!-- Mobile Dropdown Content -->
       <div class="dropdown-content" :class="{ 'visible': mobileMenuActive }">
-        <div class="close-button" @click="toggleMobileMenu">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M18 6L6 18" stroke="#133882" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M6 6L18 18" stroke="#133882" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-        </div>
         <ul class="dropdown-list">
           <li class="dropdown-item">
             <a href="#" class="dropdown-link">Home</a>
@@ -184,7 +178,7 @@ onUnmounted(() => {
 
 .logo img {
   display: block;
-  width: 36px;
+  width: 24px;
   height: auto;
 }
 
