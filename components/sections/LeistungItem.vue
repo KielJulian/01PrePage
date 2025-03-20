623ed9d5ddad121dc1bb08de958b5e9659c90b7b
kieljulian <julian.kiel@gmail.com>
2025-03-20 13:23:31 +0100

    Remove Icon and cta from leistungsitem.vue

--- components/sections/LeistungItem.vue
+++ components/sections/LeistungItem.vue
@@ -1,14 +1,7 @@
 <template>
   <BaseCard :elevated="true" class="leistung-item">
-    <div class="icon-container" :style="{ backgroundColor: iconBgColor }">
-      <img v-if="icon" :src="icon" :alt="title" class="icon">
-      <div v-else class="icon-placeholder"></div>
-    </div>
     <h3 class="title">{{ title }}</h3>
     <p class="description">{{ description }}</p>
-    <div class="cta" v-if="ctaText">
-      <a href="#" class="learn-more">{{ ctaText }} →</a>
-    </div>
   </BaseCard>
 </template>
 
@@ -16,14 +9,6 @@
 import BaseCard from '../core/BaseCard.vue';
 
 defineProps({
-  icon: {
-    type: String,
-    default: ''
-  },
-  iconBgColor: {
-    type: String,
-    default: 'var(--color-highlight-mint)'
-  },
   title: {
     type: String,
     required: true
@@ -31,10 +16,6 @@ defineProps({
   description: {
     type: String,
     required: true
-  },
-  ctaText: {
-    type: String,
-    default: 'Mehr erfahren'
   }
 });
 </script>
@@ -46,30 +27,6 @@ defineProps({
   flex-direction: column;
 }
 
-
-.icon-container {
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: var(--spacing-md);
-}
-
-.icon {
-  width: 35px;
-  height: 35px;
-  object-fit: contain;
-}
-
-.icon-placeholder {
-  width: 35px;
-  height: 35px;
-  background-color: rgba(255, 255, 255, 0.5);
-  border-radius: 50%;
-}
-
 .title {
   color: var(--color-primary);
   font-size: 1.5rem;
@@ -78,17 +35,6 @@ defineProps({
 
 .description {
   flex-grow: 1;
-  margin-bottom: var(--spacing-md);
   color: #555;
-}
-
-.learn-more {
-  color: var(--color-primary);
-  font-weight: 600;
-  transition: color 0.3s ease;
-}
-
-.learn-more:hover {
-  color: var(--color-accent-green);
 }
 </style> 
