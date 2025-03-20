0f77c3b3da52f52852a0b265eff66d24e36f1f6d
kieljulian <julian.kiel@gmail.com>
2025-03-20 22:17:54 +0100

    Fix the nuxt.config modules name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['motion-v'],
+  modules: ['motion-v/nuxt'],
   app: {
     head: {
       meta: [
